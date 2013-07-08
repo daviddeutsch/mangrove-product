ebe64a87830cf3f99b0dff0525b3b649e01bf2c3
David Deutsch <skore@valanx.org>
2013-07-08 13:46:54 +0200

    quick deployment script

---
+++ deploy.php
@@ -0,0 +1,17 @@
+<?php
+
+if ( !file_exists( __DIR__.'/../config.json' ) ) {
+	header("HTTP/1.0 401 Unauthorized"); exit;
+}
+
+$json = json_decode( file_get_contents(__DIR__.'/../config.json') );
+
+if ( !isset( $_GET['auth'] ) || !isset( $json->auth ) ) {
+	header("HTTP/1.0 401 Unauthorized"); exit;
+}
+
+if ( $_GET['auth'] != $json->auth ) {
+	header("HTTP/1.0 401 Unauthorized"); exit;
+}
+
+shell_exec( 'git fetch origin master' );
