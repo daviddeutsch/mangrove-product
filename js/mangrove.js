9b8610729a70f6ebd385036800d41f79ed383491
David Deutsch <skore@valanx.org>
2013-07-10 18:17:17 +0200

    menu demo

--- js/mangrove.js
+++ js/mangrove.js
@@ -1,4 +1,4 @@
-var mangroveApp = angular.module("mangroveApp", ['ui.bootstrap']);
+var mangroveApp = angular.module("mangroveApp", ['ui.bootstrap', 'ui.scrollfix']);
 
 function CollapseCtrl($scope) {
 	$scope.isCollapsed = false;
