9c11e7b0c9457bb336de78ee71483768a5e891e6
David Deutsch <skore@valanx.org>
2013-07-12 13:07:06 +0200

    reworking some text, starting to use a tab controller for points, preparing for use of noun project nouns, also working on color

--- js/mangrove.js
+++ js/mangrove.js
@@ -3,3 +3,7 @@ var mangroveApp = angular.module("mangroveApp", ['ui.bootstrap', 'ui.scrollfix']
 function CollapseCtrl($scope) {
 	$scope.isCollapsed = false;
 }
+
+var TabsCtrl = function ($scope) {
+	$scope.navType = 'tabs';
+};
