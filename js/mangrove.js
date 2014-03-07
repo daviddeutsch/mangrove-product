3bc1ddfa0b5b3a5b89eb1d242054dba58614b11f
David Deutsch <skore@valanx.org>
2014-03-08 00:44:50 +0100

    *sigh*

--- js/mangrove.js
+++ js/mangrove.js
@@ -27,7 +27,7 @@ $( document ).ready(function() {
 
 //$('figure').slideUp();
 
-var mangroveApp = angular.module("mangroveApp", ['ui.bootstrap', 'ui.scrollfix'])
+var mangroveApp = angular.module("mangroveApp", ['ui.bootstrap', 'ui.scrollfix', 'scrollto'])
 	.config(
 		['$locationProvider',
 		function( $locationProvider ) {
