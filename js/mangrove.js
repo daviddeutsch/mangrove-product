b62ea7127557efc7d7da38001493decbec27a883
David Deutsch <skore@valanx.org>
2014-03-08 17:05:32 +0100

    trying a custom directive instead

--- js/mangrove.js
+++ js/mangrove.js
@@ -103,7 +103,8 @@ mangroveApp
 	]
 );
 
-mangroveApp.directive('svgpoly', function ($http, $location) {
+mangroveApp
+	.directive('svgpoly', function ($http, $location) {
 	return {
 		restrict: 'E',
 		link: function ( scope, element, attrs ) {
@@ -117,6 +118,25 @@ mangroveApp.directive('svgpoly', function ($http, $location) {
 				});
 			}
 
+		}
+	}
+});
+
+mangroveApp
+	.directive('scrollOnClick', function() {
+	return {
+		restrict: 'A',
+		link: function(scope, $elm, attrs) {
+			var idToScroll = attrs.href;
+			$elm.on('click', function() {
+				var $target;
+				if (idToScroll) {
+					$target = $(idToScroll);
+				} else {
+					$target = $elm;
+				}
+				$("body").animate({scrollTop: $target.offset().top}, "slow");
+			});
 		}
 	}
 });
