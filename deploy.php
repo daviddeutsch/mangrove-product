0356583c7bf5ec357b44d63d1440529b6c8c199b
David Deutsch <skore@valanx.org>
2014-06-17 01:10:35 +0200

    cleanup

--- deploy.php
+++ deploy.php
@@ -1,18 +1,14 @@
 <?php
 
-if ( !file_exists( __DIR__.'/../config.json' ) ) {
-	header("HTTP/1.0 401 Unauthorized"); exit;
-}
+function unauthorizedDeploy() { header("HTTP/1.0 401 Unauthorized"); exit; }
+
+if ( !file_exists( __DIR__.'/../config.json' ) ) unauthorizedDeploy();
 
 $json = json_decode( file_get_contents(__DIR__.'/../config.json') );
 
-if ( !isset( $_GET['auth'] ) || !isset( $json->auth ) ) {
-	header("HTTP/1.0 401 Unauthorized"); exit;
-}
+if ( !isset( $_GET['auth'] ) || !isset( $json->auth ) ) unauthorizedDeploy();
 
-if ( $_GET['auth'] != $json->auth ) {
-	header("HTTP/1.0 401 Unauthorized"); exit;
-}
+if ( $_GET['auth'] != $json->auth ) unauthorizedDeploy();
 
 shell_exec( 'git pull origin master' );
 
